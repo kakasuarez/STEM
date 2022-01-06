355c321ddebcff0fb1c84176d70c932d70269f5a
Lakshya Vohra <lakshyavohra09@gmail.com>
2022-01-06 14:22:26 +0530

    Co-authored-by: Vatsal Veerwal <vatsalveerwal@gmail.com> added backgrounds

--- js/technology.js
+++ js/technology.js
@@ -1,5 +1,10 @@
-const x = document.getElementById("body")
-x.classList.add("active");
+const earlierHTML = document.documentElement.innerHTML;
+document.documentElement.innerHTML = "";
+const styleEl = document.createElement("link");
+styleEl.rel = "stylesheet";
+styleEl.href = "../css/technology.css";
+document.head.appendChild(styleEl);
+document.body.classList.add("bg-img");
 setTimeout(() => {
-    
-}, 1000)
+    document.documentElement.innerHTML = earlierHTML;
+}, 4500);
