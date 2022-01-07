21d23ac7d0c582eaeca6135bda1347a62a262ea7
Vatsal Veerwal <vatsalveerwal@gmail.com>
2022-01-07 15:43:41 +0530

    Co-authored-by: LakshyaVohra <LakshyaVohra@users.noreply.github.com> Improved animation

--- js/science.js
+++ js/science.js
@@ -1,29 +1,47 @@
-function animate() {
-    var tl = anime.timeline({
-        // easing: 'easeOutExpo',
-        duration: 4000,
-        loop: true
-    });
-    tl.add({
-        targets: '.extremeleft',
-        keyframes: [
-            {translateY: -40, translateX: -75},
-            {translateX: 0, translateY: 0},
-        ],
-        duration: 1000,
-        easing: 'linear',
-        loop: true
-    })
-    .add({
-        targets: '.extremeright',
-        keyframes: [
-            {translateY: -40, translateX: 75},
-            {translateX: 0, translateY: 0},
-        ],
-        duration: 1000,
-        easing: 'linear',
-        loop: true
-    })
+const control = document.querySelector('.control');
+
+const im = document.createElement("img");
+im.classList.add("control-im");
+control.appendChild(im);
+
+
+const tl = anime.timeline({
+    duration: 4000,
+    loop: true
+});
+tl.add({
+    targets: '.extremeleft',
+    keyframes: [
+        {translateY: -40, translateX: -75},
+        {translateX: 0, translateY: 0},
+    ],
+    duration: 1000,
+    easing: 'linear',
+    loop: true
+})
+.add({
+    targets: '.extremeright',
+    keyframes: [
+        {translateY: -40, translateX: 75},
+        {translateX: 0, translateY: 0},
+    ],
+    duration: 1000,
+    easing: 'linear',
+    loop: true
+})
+
+
+function play() {
+    tl.play();
+    control.onclick = pause;
+    im.src = "../images/pause.png";
 }
 
-animate();
+function pause() {
+    tl.pause();
+    control.onclick = play;
+    im.src = "../images/play.png";
+}
+
+im.src = "../images/pause.png";
+control.onclick = pause;
