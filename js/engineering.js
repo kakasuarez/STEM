d95f469af2ff15dee7bccb68fcc6c1b67256614d
Lakshya Vohra <lakshyavohra09@gmail.com>
2022-01-11 15:00:35 +0530

    gear animation
    Co-authored-by: Vatsal Veerwal <vatsalveerwal@gmail.com>

--- js/engineering.js
+++ js/engineering.js
@@ -1,4 +1,20 @@
-/* const paths = document.getElementsByTagName("path");
-for (let i=0; i<paths.length; i++) {
-console.log(`Letter ${i} is ${paths[i].getTotalLength()}`);
-} */
+const gear = document.querySelector(".gear");
+const gearButton = document.querySelector(".gear button");
+const gearContainer = document.querySelector(".gear-container");
+const gearText = document.getElementById("gear-text");
+
+gear.addEventListener("click", () => {
+    gearText.style.display = "block";
+    gearButton.style.display = "none";
+})
+
+const showGear = () => {
+    gearContainer.style.visibility = "visible";
+}
+
+const showGearButton = () => {
+    gearButton.style.display = "block";
+}
+
+setTimeout(showGear, 3000);
+setTimeout(showGearButton, 4000);
