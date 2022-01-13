35d0998cab52175765abdaee49d48d5ff55c12e0
Lakshya Vohra <lakshyavohra09@gmail.com>
2022-01-13 14:58:58 +0530

    final touches

--- js/engineering.js
+++ js/engineering.js
@@ -1,20 +1,13 @@
-const gear = document.querySelector(".gear");
-const gearButton = document.querySelector(".gear button");
 const gearContainer = document.querySelector(".gear-container");
 const gearText = document.getElementById("gear-text");
-
-gear.addEventListener("click", () => {
-    gearText.style.display = "block";
-    gearButton.style.display = "none";
-})
 
 const showGear = () => {
     gearContainer.style.visibility = "visible";
 }
 
-const showGearButton = () => {
-    gearButton.style.display = "block";
+const showGearText = () => {
+    gearText.style.display = "block";
 }
 
 setTimeout(showGear, 1000);
-setTimeout(showGearButton, 2000);
+setTimeout(showGearText, 2000);
