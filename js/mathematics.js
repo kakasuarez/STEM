3fcd60eeed4cf7fd03df2c7a2d5b53478f3fbadd
Lakshya Vohra <lakshyavohra09@gmail.com>
2022-01-13 14:38:31 +0530

    maths done
    Co-authored-by: Vatsal Veerwal <vatsalveerwal@gmail.com>

--- js/mathematics.js
+++ js/mathematics.js
@@ -1,39 +1,45 @@
-/* new Chart(document.getElementById("myChart"), {
-    type: 'line',
-    data: {
-      labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-      datasets: [{ 
-          data: [86,114,106,106,107,111,133,221,783,2478],
-          label: "Africa",
-          borderColor: "#3e95cd",
-          fill: false
-        }, { 
-          data: [282,350,411,502,635,809,947,1402,3700,5267],
-          label: "Asia",
-          borderColor: "#8e5ea2",
-          fill: false
-        }, { 
-          data: [168,170,178,190,203,276,408,547,675,734],
-          label: "Europe",
-          borderColor: "#3cba9f",
-          fill: false
-        }, { 
-          data: [40,20,10,16,24,38,74,167,508,784],
-          label: "Latin America",
-          borderColor: "#e8c3b9",
-          fill: false
-        }, { 
-          data: [6,3,2,2,7,26,82,172,312,433],
-          label: "North America",
-          borderColor: "#c45850",
-          fill: false
+const ctx = document.getElementById("myChart").getContext('2d');
+const number = document.getElementById("number");
+const color = document.getElementById("color");
+
+const myChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ["Green", "Blue", "Purple", "Yellow", "Red"],
+    datasets: [{
+      backgroundColor: [
+        "green",
+        "blue",
+        "purple",
+        "yellow",
+        "red",
+      ],
+      data: [20, 20, 20, 20, 20],
+      borderColor: "black"
+    }]
+},
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          color: "white"
         }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'World population per region (in millions)'
       }
     }
-  }); */
+  }
+});
+
+function changeChart() {
+  chosenColor = color.value;
+  chosenNumber = parseInt(number.value);
+  if (chosenNumber > 100 || chosenNumber < 0) {
+    return;
+  }
+  const index = myChart.data.labels.indexOf(chosenColor);
+  myChart.data.datasets[0].data[index] = chosenNumber;
+  myChart.update();
+}
+
+
+number.addEventListener("input", changeChart)
+color.addEventListener("input", changeChart)
